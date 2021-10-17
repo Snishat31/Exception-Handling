009acd275715a6e4da34c8c6e018454cabc979c9
Syeda Nishat <syedanishat.gmail.com>
2021-10-17 14:05:53 +0300

    Exception sample program

---
+++ tryCatchSample.java
@@ -0,0 +1,23 @@
+package ExceptionHandling;
+
+public class tryCatchSample {
+
+    public static void main(String args[]) 
+    {
+        int[] array = new int[7];
+        try 
+        {
+            array[7] = 3;
+        }
+        catch (ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("Array index is out of bounds!"); 
+        }
+        finally 
+        {
+            System.out.println("The array is of size " + array.length);
+        }
+    }
+
+
+}
